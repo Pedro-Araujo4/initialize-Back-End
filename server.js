9acdfa547d743914e9724aa5d2a2206c6817427d
Pedro Araujo <pa298571@gmail.com>
2024-11-22 10:41:50 -0300

    1

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 import express from "express";
-import routes from "./src/routes/postsRoutes.js";
+import conectarAoBanco from "./src/config/dbConfig.js";
+await conectarAoBanco(process.env.STRING_CONEXAO)
+
 
 const app = express();
-
-routes(app)
+app.use(express.json());
 
 app.listen(3000, () => {
     console.log("Servidor escutando...");
 });
+
+
 
 
 
--- src/controllers/postsController.js
+++
@@ -1,6 +0,0 @@
-import getAllPosts from "../models/postsModel.js"
-
-export async function listPosts(req, res) {
-    const posts = await getAllPosts();
-    res.status(200).json(posts)
-}
