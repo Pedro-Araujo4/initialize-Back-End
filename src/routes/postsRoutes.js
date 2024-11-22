3c7cb24d6ebd81cafaa914fdf75ff646865b6d43
Pedro Araujo <pa298571@gmail.com>
2024-11-22 11:24:16 -0300

    aula03

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,9 +1,18 @@
 import express from "express";
 import { listPosts } from "../controller/postsController.js";
 
+export default function routes(app) {
+    app.use(express.json());
+    app.get("/posts", listPosts)
+}
+
+//ou
+
+/*
 const routes = (app) => {
     app.use(express.json());
     app.get("/posts", listPosts)
 }
 
 export default routes;
+*/
